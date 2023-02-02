863899c3545f383b29e17a322a8ed0bb67975d09
Carlos Adriano Portes <xcap2000@outlook.com>
2023-02-02 13:27:18 -0300

    Add new layout repository

---
+++ AlignedMaterialWriteRepository.cs
@@ -0,0 +1,49 @@
+namespace ClashCodes
+{
+    public class AlignedMaterialWriteRepository
+    {
+        private readonly string filename;
+
+        public AlignedMaterialWriteRepository(string filename)
+        {
+            this.filename = filename;
+            if (File.Exists(filename))
+            {
+                File.Delete(filename);
+            }
+            File.AppendAllText(filename, $"Matnr,Desc,Matnr,Desc{Environment.NewLine}");
+        }
+
+        public void Add(List<Material> materials)
+        {
+            for (int index = 1; index < materials.Count; index++)
+            {
+                if (index == 1)
+                {
+                    var firstMaterial = materials[0];
+                    var secondMaterial = materials[index];
+                    var firstDescription = FormatDescription(firstMaterial);
+                    var secondDescription = FormatDescription(secondMaterial);
+
+                    var line = $"{firstMaterial.Id},{firstDescription},{secondMaterial.Id},{secondDescription}{Environment.NewLine}";
+                    File.AppendAllText(filename, line);
+                }
+                else
+                {
+                    var currentMaterial = materials[index];
+                    var currentDescription = FormatDescription(currentMaterial);
+
+                    var line = $",,{currentMaterial.Id},{currentDescription}{Environment.NewLine}";
+                    File.AppendAllText(filename, line);
+                }
+            }
+        }
+
+        private static string FormatDescription(Material material)
+        {
+            return material.Description.Contains(',')
+                ? "\"" + material.Description + "\""
+                : material.Description;
+        }
+    }
+}
